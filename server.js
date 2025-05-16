4d6136fd8350cce750446180c269cd2327dd5d4e
Lucas Dircksen Balcevicz <145569604+Lucas-dirbal@users.noreply.github.com>
2025-05-16 02:16:55 +0000

    a

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 const express = require('express');
-const sendMessageHandler = require('./sendMessage'); // seu arquivo de envio
+const sendMessageHandler = require('./api/sendMenssage'); // ajuste aqui o caminho
 
 require('dotenv').config({ path: './bot.env' });
 
 const app = express();
 const PORT = 3000;
 
-// Para entender JSON no body das requisições POST
 app.use(express.json());
-
-// Servir arquivos estáticos (como seu HTML, CSS, imagens)
 app.use(express.static(__dirname));
 
-// Rota que recebe os dados do formulário e envia para o Telegram
 app.post('/api/send-message', sendMessageHandler);
 
-// Inicia o servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
